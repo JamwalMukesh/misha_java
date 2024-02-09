400d14fcf8d69a8a38e5090403af8fc293073ae2
MukeshJamwal <mukeshjamwal@trigvent.com>
2024-02-09 21:45:07 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Hello World ");
+        System.out.println(" Hello World ");
+        System.out.print(" Hello World ");
+        System.out.print(" Hello World ");
+        System.out.println(" Hello World \u0045 \u0915 ");
+    }
+}
