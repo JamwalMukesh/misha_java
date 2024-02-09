2766cda9dce1558a1ef4fca570d9f2fa50a6a12d
MukeshJamwal <mukeshjamwal@trigvent.com>
2024-02-09 22:10:25 +0530

    day 1

---
+++ day1/Second.java
@@ -0,0 +1,14 @@
+// Java Script is a Dynamically Type Initialization
+// What about Type Script is type safe java script
+// In Java Script, debugging is tough.
+// In Java, it is a type safe language which means we need to declare its type of variable before using it
+// In other word we can say Java is Static Type Initializer
+// In Java, most basic error is captured at compile time.
+// Debate
+class Second
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
