34bbdffbee03192833d7cd688e8e1b6b27de8faa
MukeshJamwal <mukeshjamwal@trigvent.com>
2024-02-15 21:40:16 +0530

    day 4

---
+++ day4/First.java
@@ -0,0 +1,11 @@
+class First
+{
+    public static void main(String[] args) {
+        System.out.println("Square root of 3: " + Math.sqrt(3));
+        System.out.println("Power of 2 raise to 3: " + Math.pow(2,3));
+        System.out.println("Ceil of 2.5: " + Math.ceil(2.5));
+        System.out.println("Floor of 2.5: " + Math.floor(2.5));
+        System.out.println("Log10 of 10: " + Math.log10(10));
+        System.out.println("Log10 of 1000: " + Math.log10(1000));
+    }
+}
