173506457cf357befb044b5df1eb4404a256d440
MukeshJamwal <mukeshjamwal@trigvent.com>
2024-05-07 21:18:25 +0530

    day 35

---
+++ day35/demo/First.java
@@ -0,0 +1,13 @@
+package demo;
+
+// CLASSPATH means where java search byte code.
+// Byte code file name have extension .class
+
+
+public class First
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello From Package demo");
+    }
+}
